63d70c16089fdec750cf1c865333913142ad11e7
amandaseara <amanda.seara@correo.ucu.edu.uy>
2022-05-29 00:38:00 -0300

    Clases Iimpresora, ImpresoraConsola y TraductorCoordenadas

--- src/Library/ImpresoraConsola.cs
+++ src/Library/ImpresoraConsola.cs
@@ -13,9 +13,17 @@ namespace ClassLibrary
         /// <param name="tablero"></param>
         void ImprimirTablero(char[,] tablero)
         {
-            string filaImprimir = " ";
-            string[] letras = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O"];
-            string[] numeros = ["1","2","3","4","5","6","7","8","9","10","11","12","13","14","15"];
+            string filaImprimir = "  ";
+            string letras = "ABCDEFGHIJKLMNO";
+            string[] numeros = new string[15];
+            for (int j = 0 ; j<9 ; j++)
+            {
+                numeros[j] = $" {j + 1}";
+            }
+            for (int j = 9 ; j<numeros.Length ; j++)
+            {
+                numeros[j] = $"{j + 1}";
+            }
             for (int i = 0; i<tablero.GetLength(1); i++)
             {
                 filaImprimir = filaImprimir + " " + letras[i];
@@ -35,18 +43,18 @@ namespace ClassLibrary
         /// Imprime los datos publicos de los usuarios en consola
         /// </summary>
         /// <param name="perfil"></param>
-        void ImprimirPerfilUsuario(PerfilUsuario perfil)
+        /*void ImprimirPerfilUsuario(PerfilUsuario perfil)
         {
             Console.WriteLine($"Nombre: {perfil.Nombre}");
             Console.WriteLine($"Ganadas: {perfil.Ganadas}");
             Console.WriteLine($"Nombre: {perfil.Perdidas}");
-        }
+        }*/
         /// <summary>
         /// Imprime el historial de todas las partidas jugadas en consola
         /// </summary>
         void ImprimirHistorial()
         {
-            foreach (Partida partida in Admin.ObtenerHistorial())
+            /*foreach (Partida partida in Admin.ObtenerHistorial())
             {
                 foreach (Tablero tablero in partida)
                 {
@@ -54,19 +62,19 @@ namespace ClassLibrary
                 }
                 Console.WriteLine($"Ganador: {Admin.ObtenerPerfil(partida.Ganador).Nombre}");
                 Console.WriteLine($"Ganador: {Admin.ObtenerPerfil(partida.Perdedor).Nombre}");
-            }
+            }*/
         }
         /// <summary>
         /// Imprime en consola un rancking, en el que los perfiles tienen posiciones ordenados segun batallas ganadas
         /// </summary>
         void ImprimirRanking()
         {
-            int puesto = 1;
+            /*int puesto = 1;
             foreach (PerfilUsuario perfil in Admin.ObtenerRanking())
             {
                 Console.WriteLine($"N° {puesto}: {perfil.Nombre} con {perfil.Ganadas} batallas ganadas");
                 puesto = puesto + 1;
-            }
+            }*/
         }
     }
 }
