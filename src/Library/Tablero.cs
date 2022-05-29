630c0f69696d1a677aad9354d64ce352214b49cb
Franco De Stefano <FRANCO.DESTEFANO@correo.ucu.edu.uy>
2022-05-28 21:52:09 -0300

    Arreglo un error en ActualizarTablero

--- src/Library/Tablero.cs
+++ src/Library/Tablero.cs
@@ -14,7 +14,7 @@ namespace ClassLibrary
         /// <summary>
         /// Este ArrayList de char es donde esta contenido una gran parte de los datos del juego.
         /// </summary>
-        protected char[ , ] matriz;
+        protected char[,] matriz;
         /// <summary>
         /// Este atributo contiene el numero de ataques efectuados hacia el tablero.
         /// </summary>
@@ -22,7 +22,7 @@ namespace ClassLibrary
         /// <summary>
         /// Este atributo se encarga de mostrar el estado terminado de la partida, normalmente esta en false.
         /// </summary>
-        public bool terminado = false ;
+        public bool terminado = false;
         /// <summary>
         /// En este atributo se ve el numero de jugador de quien es el dueño del tablero, osea el que puede ver la informacion de los barcos intactos principalmente.
         /// </summary>
@@ -30,7 +30,7 @@ namespace ClassLibrary
         /// <summary>
         /// Variable que facilita saber quien es el ganador de la partida cuando se almacena.
         /// </summary>
-        public bool Ganador= false;
+        public bool Ganador = false;
         /// <summary>
         /// Constructor de tableros, crea una matriz en base al tamaño que le diga quien llame al metodo
         /// </summary>
@@ -39,7 +39,7 @@ namespace ClassLibrary
         public Tablero(int Tamaño, int dueño)
         {
             this.tamaño = Tamaño;
-            this.matriz = new char [tamaño,tamaño];
+            this.matriz = new char[tamaño, tamaño];
             this.DueñodelTablero = dueño;
         }
         /// <summary>
@@ -50,7 +50,21 @@ namespace ClassLibrary
         /// <param name="nuevovalor"></param>
         public void ActualizarTablero(int filas, int columnas, char nuevovalor)
         {
-            matriz[filas,columnas] = nuevovalor;
+            if (nuevovalor == 'B')
+            {
+                matriz[filas, columnas] = nuevovalor;
+            }
+            else if (nuevovalor == 'A')
+            {
+                if (matriz[filas, columnas] == 'B' || matriz[filas, columnas] == 'T')
+                {
+                    matriz[filas, columnas] = 'T';
+                }
+                else
+                {
+                    matriz[filas, columnas] = 'W';
+                }
+            }
         }
         /// <summary>
         /// Metodo encargado de añadir un barco al tablero, se le asignan el inicio y el final,
@@ -59,29 +73,29 @@ namespace ClassLibrary
         /// </summary>
         /// <param name="InicioDeBarco"></param>
         /// <param name="FinalDeBarco"></param>
-        public void Añadirbarco( int[] InicioDeBarco, int[] FinalDeBarco)
+        public void Añadirbarco(int[] InicioDeBarco, int[] FinalDeBarco)
         {
-            if(InicioDeBarco[0]<=this.tamaño && InicioDeBarco[1]<=this.tamaño && FinalDeBarco[0]<=this.tamaño && FinalDeBarco[1]<=this.tamaño && (InicioDeBarco[0]== FinalDeBarco[0] || InicioDeBarco[1] == FinalDeBarco[1]) )
+            if (InicioDeBarco[0] <= this.tamaño && InicioDeBarco[1] <= this.tamaño && FinalDeBarco[0] <= this.tamaño && FinalDeBarco[1] <= this.tamaño && (InicioDeBarco[0] == FinalDeBarco[0] || InicioDeBarco[1] == FinalDeBarco[1]))
             {
                 if (InicioDeBarco[0] == FinalDeBarco[0])
                 {
-                    for(int i=InicioDeBarco[1]-1; i<FinalDeBarco[1]; i++)
+                    for (int i = InicioDeBarco[1] - 1; i < FinalDeBarco[1]; i++)
                     {
                         if (i >= 0)
                         {
-                           ActualizarTablero( InicioDeBarco[0], i, 'B'); 
+                            ActualizarTablero(InicioDeBarco[0], i, 'B');
                         }
-                        
+
                     }
                 }
 
                 if (InicioDeBarco[1] == FinalDeBarco[1])
                 {
-                    for(int i=InicioDeBarco[1]-1; i<FinalDeBarco[1]; i++)
+                    for (int i = InicioDeBarco[1] - 1; i < FinalDeBarco[1]; i++)
                     {
                         if (i >= 0)
                         {
-                            ActualizarTablero( i, InicioDeBarco[1], 'B');
+                            ActualizarTablero(i, InicioDeBarco[1], 'B');
                         }
                     }
                 }
@@ -92,11 +106,14 @@ namespace ClassLibrary
         /// </summary>
         /// <param name="usuarioqueconsulta"></param>
         /// <returns></returns>
-        public char[ , ] VerTablero(int usuarioqueconsulta)
+        public char[,] VerTablero(int usuarioqueconsulta)
         {
             // polimorfismo
             return matriz;
         }
+        /// <summary>
+        /// Metodo llamado para Finalizar
+        /// </summary>
         public void Finalizar()
         {
             DatosdePartida partida = new DatosdePartida();
