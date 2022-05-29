b854eaa749a86dfe3407fff5d1bc1effff8e8ef5
Franco De Stefano <FRANCO.DESTEFANO@correo.ucu.edu.uy>
2022-05-28 21:07:19 -0300

    Creo las clases de tableros y datos de partida

---
+++ src/Library/DatosdePartidas.cs
@@ -0,0 +1,80 @@
+using System;
+
+namespace ClassLibrary
+{
+    /// <summary>
+    /// Clase encargada de almacenar los datos referentes al acabar una partida
+    /// </summary>
+    public class DatosdePartida
+    {
+        /// <summary>
+        /// Lugar donde se almacenan los tableros.
+        /// </summary>
+        public Tablero[] Tableros;
+        /// <summary>
+        /// Lugar donde se almacenan los jugadores.
+        /// </summary>
+        public int[] Jugadores;
+        /// <summary>
+        /// El int es el número de jugador del perfil de usuario perteneciente al ganador.
+        /// </summary>
+        public int Ganador=0;
+        /// <summary>
+        /// El int es el número de jugador del perfil de usuario perteneciente al perdedor.
+        /// </summary>
+        public int Perdedor=0;
+        /// <summary>
+        /// Metodo encargado de almacenar los datos.
+        /// </summary>
+        /// <param name="tableroParaAgregar"></param>
+        public void Almacenar(Tablero tableroParaAgregar)
+        {
+            AlmacenarTableros(tableroParaAgregar);
+            AlmacenarJugador(tableroParaAgregar.DueñodelTablero);
+            if (tableroParaAgregar.Ganador)
+            {
+                Ganador = tableroParaAgregar.DueñodelTablero;
+            }
+            else if (!tableroParaAgregar.Ganador)
+            {
+                Perdedor = tableroParaAgregar.DueñodelTablero;
+            }
+            if (Ganador!=0 & Perdedor!=0)
+            {
+                //Historial.AlmacenarPartida(this);
+            }
+        }
+        /// <summary>
+        /// Metodo encargado de almacenar un tablero
+        /// </summary>
+        /// <param name="tableroParaAgregar"></param>
+        public void AlmacenarTableros(Tablero tableroParaAgregar)
+        {
+            if (Tableros == null)
+            {
+                Tableros = new Tablero[2];
+                Tableros[0] = tableroParaAgregar;
+            }
+            else
+            {
+                Tableros[1] = tableroParaAgregar;
+            }
+        }
+        /// <summary>
+        /// Metodo encargado de almacenar un jugador
+        /// </summary>
+        /// <param name="jugador"></param>
+        public void AlmacenarJugador(int jugador)
+        {
+            if (Jugadores == null)
+            {
+                Jugadores = new int[2];
+                Jugadores[0] = jugador;
+            }
+            else
+            {
+                Jugadores[1] = jugador;
+            }
+        }
+    }
+}
