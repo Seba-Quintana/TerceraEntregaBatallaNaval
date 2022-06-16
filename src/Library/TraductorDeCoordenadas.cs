e674d8beee670401c8658b94209b074d05bf4971
Seba-Quintana <SEBASTIAN.QUINTANA@correo.ucu.edu.uy>
2022-06-16 19:30:31 -0300

    arreglo de traductor y excepcion de emparejamiento

--- src/Library/TraductorDeCoordenadas.cs
+++ src/Library/TraductorDeCoordenadas.cs
@@ -15,6 +15,15 @@ namespace ClassLibrary
         /// <returns></returns>
         public static int[] Traducir(string coordenada)
         {
+            try
+            {
+                if ((coordenada.Length != 2) && (coordenada.Length != 3))
+                   throw new ArgumentOutOfRangeException(coordenada);
+            }
+            catch (ArgumentOutOfRangeException ex)
+            {
+                throw ex;
+            }
             int[] traducido = new int[2];
             List<string> letras = new List<string> () {"A","B","C","D","E","F","G","H","I","J","K","L","M","N","O"};
             int i = 0;
@@ -22,8 +31,16 @@ namespace ClassLibrary
             {
                 i = i + 1;
             }
-            traducido[0] = i;
-            traducido[1] = (int)Char.GetNumericValue(coordenada[1]) - 1;
+            if (coordenada.Length == 2)
+            {
+                traducido[0] = i;
+                traducido[1] = (int)Char.GetNumericValue(coordenada[1]) - 1;
+            }
+            else
+            {
+                traducido[0] = i;
+                traducido[1] = (int)Char.GetNumericValue(coordenada[1])*10 + (int)Char.GetNumericValue(coordenada[2]) - 1;
+            }
             return traducido;
         }
     }
