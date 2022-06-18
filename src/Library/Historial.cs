3d072ba7e6363d086dc739c56f328bcf39163b11
Seba-Quintana <101676569+Seba-Quintana@users.noreply.github.com>
2022-06-18 19:19:04 -0300

    Update Historial.cs

--- src/Library/Historial.cs
+++ src/Library/Historial.cs
@@ -4,20 +4,22 @@ using System.Collections.Generic;
 namespace ClassLibrary
 {
     /// <summary>
-    /// Esta clase se encarga de añadir las partidas que se jugaron a la lista que se define posteriormente.
+    /// Esta clase se encarga de añadir las partidas que se jugaron a una lista,
+    /// con el objetivo de mantener un registro de las partidas jugadas.
     /// </summary>
     public static class Historial
     {
       /// <summary>
-      /// Creamos una lista llamada partidas, cuyo tipo es <DatosdePartida> que es donde se almacenará las partidas que
-      /// hemos jugado independientemente en cuanto a si hemos ganado o perdido.
+      /// Creamos una lista llamada partidas, la cual contiene elementos del tipo DatosdePartida, que es donde se almacenan las partidas que
+      /// se han jugado.
       /// </summary>
       public static List<DatosdePartida> partidas;
+        
       /// <summary>
-      /// Este método básicamente se encarga de almacenar la partida en la lista que hemos creado anteriormente,
-      /// luego de finalizar la partida, guardándose en el historial de nuestro usuario
+      /// Este método se encarga de almacenar las partidas jugadas en la lista de partidas
+      /// luego de finalizadas.
       /// </summary>
-      /// <param name="partida"></param>
+      /// <param name="partida"> Partida a almacenar </param>
       public static void AlmacenarPartida(DatosdePartida partida)
       {
         partidas.Add(partida);
