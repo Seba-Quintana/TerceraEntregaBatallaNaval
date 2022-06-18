afde7c01944e3c6943e1030577af70219a352fa3
[Santiago Severo] <[santiagosevero28@gmail.com]>
2022-06-18 18:39:41 -0300

    Se agregaron comentarios en las clases Historial, Iimpresora y ImpresoraConsola.

--- src/Library/Historial.cs
+++ src/Library/Historial.cs
@@ -8,8 +8,16 @@ namespace ClassLibrary
     /// </summary>
     public static class Historial
     {
+      /// <summary>
+      /// Creamos una lista llamada partidas, cuyo tipo es <DatosdePartida> que es donde se almacenará las partidas que
+      /// hemos jugado independientemente en cuanto a si hemos ganado o perdido.
+      /// </summary>
       public static List<DatosdePartida> partidas;
-    
+      /// <summary>
+      /// Este método básicamente se encarga de almacenar la partida en la lista que hemos creado anteriormente,
+      /// luego de finalizar la partida, guardándose en el historial de nuestro usuario
+      /// </summary>
+      /// <param name="partida"></param>
       public static void AlmacenarPartida(DatosdePartida partida)
       {
         partidas.Add(partida);
--- src/Library/Iimpresora.cs
+++ src/Library/Iimpresora.cs
@@ -15,22 +15,28 @@ namespace ClassLibrary
     public interface Iimpresora
     {
         /// <summary>
-        /// 
+        /// Esta operación se encarga de Imprimir el tablero del juego, teniendo como parametros a 
+        /// una matriz de caracteres de tablero y una valor booleano de jugador.
         /// </summary>
         /// <param name="tablero"></param>
         public void ImprimirTablero(char[,] tablero, bool jugador);
         /// <summary>
-        /// 
+        /// Esta operación se encarga de imprimir el perfil del usuario, teniendo como parámetro 
+        /// a un perfil de tipo PerfilUsuario. Esto nos servirá para ver los tableros al disputarse las partidas.
         /// </summary>
         /// <param name="perfil"></param>
         public void ImprimirPerfilUsuario(PerfilUsuario perfil);
         /// <summary>
-        /// 
-        /// </summary>
+         /// Esta operción se encarga de imprimir al historial, que recibe como 
+        /// parámetro una lista llamada partidas tipo <DatosdePartida>, nos mostrará el perfil que quiéramos ver, si es 
+        /// que existe este. 
         /// <param name="partidas"></param>
         public void ImprimirHistorial(List<DatosdePartida> partidas);
         /// <summary>
-        /// 
+        /// Esta operción se encarga de imprimir el ranking de jugadores, 
+        /// recibiendo como parametro una lista llamada perfiles tipo <PerfilUsuario>, 
+        /// dentro de esa lista se ordenará los usuarios con sus respectivas victorias, derrotas y 
+        /// partidas jugadas tomando estas para que cada usuario sea capáz de obtener un puesto dentro del Ranking  
         /// </summary>
         /// <param name="perfiles"></param>
         public void ImprimirRanking(List<PerfilUsuario> perfiles);
