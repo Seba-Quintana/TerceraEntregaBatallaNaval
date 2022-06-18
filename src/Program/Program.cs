20d9d061e8d22d5517406cddb5e61bc291b619ab
Seba-Quintana <SEBASTIAN.QUINTANA@correo.ucu.edu.uy>
2022-06-18 00:16:46 -0300

    mensajesParaJugador

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -30,18 +30,18 @@ namespace ConsoleApplication
             //jugador3.VerPerfil(jugador4.NumeroDeJugador);
             jugador1.PartidaAmistosa(0, jugador2.NumeroDeJugador, 7);
             //jugador1.VisualizarTablero();
-            jugador1.PosicionarBarcos("A1","A6");
-            jugador1.PosicionarBarcos("B1","B6");
-            jugador1.PosicionarBarcos("C1","C6");
-            jugador1.PosicionarBarcos("D1","D6");
-            jugador2.PosicionarBarcos("E1","E6");
-            jugador2.PosicionarBarcos("F1","F6");
-            jugador2.PosicionarBarcos("G1","G6");
-            jugador2.PosicionarBarcos("A7","G7");
-            jugador1.Atacar("A1");
-            jugador2.Atacar("G1");
-            jugador1.Atacar("G1");
-            jugador2.Atacar("A1");
+            Console.WriteLine(jugador1.PosicionarBarcos("A1","A6"));
+            Console.WriteLine(jugador1.PosicionarBarcos("B1","B6"));
+            Console.WriteLine(jugador1.PosicionarBarcos("C1","C6"));
+            Console.WriteLine(jugador1.PosicionarBarcos("D1","D6"));
+            Console.WriteLine(jugador2.PosicionarBarcos("E1","E6"));
+            Console.WriteLine(jugador2.PosicionarBarcos("F1","F6"));
+            Console.WriteLine(jugador2.PosicionarBarcos("G1","G6"));
+            Console.WriteLine(jugador2.PosicionarBarcos("A7","G7"));
+            Console.WriteLine(jugador1.Atacar("A1"));
+            Console.WriteLine(jugador2.Atacar("G1"));
+            Console.WriteLine(jugador1.Atacar("G1"));
+            Console.WriteLine(jugador2.Atacar("A1"));
             //SE VEN SOLO TRES ATAQUES DE LOS 4
             jugador2.VisualizarTablero();*/
         }
