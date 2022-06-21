5cc695d4cc964eeb8cfdec801f06640dd9f67c9c
[Santiago Severo] <[santiagosevero28@gmail.com]>
2022-06-21 07:57:13 -0300

    Se realizó comentarios a los Test

--- test/LibraryTests/JugadorTests.cs
+++ test/LibraryTests/JugadorTests.cs
@@ -5,18 +5,14 @@ using NUnit.Framework;
 
 namespace Tests
 {
+    /// <summary>
+    /// Se crea una clase para poder realizar los test que sean posibles de la clase Jugador
+    /// </summary>
     public class JugadorTests
     {
         private Jugador jugador;
 
 
-        public List<Jugador> jugadores = new List<Jugador>();
-    
-        [SetUp]
-        public void Setup()
-        {
-            List<Jugador> jugadores = new List<Jugador>();
-        }
 
         /// <summary>
         /// Comparo si el número de jugador antes del registro y el número de jugador después del registro son iguales.
@@ -33,7 +29,9 @@ namespace Tests
             int actual = perfil.ObtenerPerfil(jugador1.NumeroDeJugador).NumeroDeJugador; 
             Assert.AreEqual(expected,actual);
         }
-        
+        /// <summary>
+        /// Se realiza test para comprobar si un jugador se remueve.
+        /// </summary>
         [Test]
         public void Remover()
         {
@@ -48,7 +46,9 @@ namespace Tests
             Assert.AreEqual(expected,actual);
             
         }
-
+        /// <summary>
+        /// Se realiza test para comprobar si un jugador mantiene su número de Jugador removiendo a otro Jugador.
+        /// </summary>
         [Test]
         public void RemoverConNum()
         {
@@ -63,7 +63,9 @@ namespace Tests
             Assert.AreEqual(expected,actual);
             
         }
-
+        /// <summary>
+        /// Con este test podremos ver el Perfil de un jugador utilizando a su Número de Jugador el cual lo tiene definido.
+        /// </summary>
         [Test]
         public void VerAlPerfil()
         {
@@ -72,36 +74,11 @@ namespace Tests
             Jugador jugador2 = new Jugador("Calamardo",25,"pez");
             Jugador jugador3 = new Jugador("Martin",56,"pildora");
             Jugador jugador4 = new Jugador("Valentino",46,"yamaha");
-            int expected = jugador1.NumeroDeJugador;
-            jugador1.VerPerfil(jugador1.NumeroDeJugador);
             int actual = jugador1.NumeroDeJugador;
+            jugador1.VerPerfil(jugador1.NumeroDeJugador);
+            int expected = 12;
             Assert.AreEqual(expected,actual);
-
-            
-
-            
-            
         }
     
-        [Test]
-        public void VerAlRanking()
-        {
-            AlmacenamientoUsuario verranking = AlmacenamientoUsuario.Instance();
-            Jugador jugador1 = new Jugador("Samuel",12,"milanesa");
-            Jugador jugador2 = new Jugador("Calamardo",25,"pez");
-            Jugador jugador3 = new Jugador("Martin",56,"pildora");
-            Jugador jugador4 = new Jugador("Valentino",46,"yamaha");
-            List<PerfilUsuario> ranking = verranking.ObtenerRanking();
-            List<PerfilUsuario> actual = ranking;
-            List<PerfilUsuario> expected = ranking;
-            Assert.AreEqual(expected,actual);
-
-
-
-            
-
-            
-            
-        }
     }
 }
