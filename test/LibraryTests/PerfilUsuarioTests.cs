64e450bb36b2199133a3fe79a1698fa8feb3d7d3
[Santiago Severo] <[santiagosevero28@gmail.com]>
2022-06-21 21:25:07 -0300

    Se testea PerfilUsuario, el método testeado es ObtenerHistorialPersonal

---
+++ test/LibraryTests/PerfilUsuarioTests.cs
@@ -0,0 +1,77 @@
+using System;
+using ClassLibrary;
+using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace Tests
+{
+	/// <summary>
+	/// Tests de AlmacenamientoUsuarios <see cref="AlmacenamientoUsuario"/>
+	/// </summary>
+    [TestFixture]
+    public class PerfilUsuarioTests
+    {
+        private Jugador jugador;
+
+        private int ganadas = 0;
+
+        private int perdidas = 0;
+
+        private List<DatosdePartida> HistorialPersonal = new List<DatosdePartida>();
+
+        List<PerfilUsuario> ListadeUsuarios = new List<PerfilUsuario>();
+
+
+
+        [Test]
+        public void obtenerHistorialPersonal()
+        {
+            AlmacenamientoUsuario almacenamiento = AlmacenamientoUsuario.Instance();
+            Jugador jugador1 = new Jugador("Carlos",67,"player1");
+            Jugador jugador2 = new Jugador("Drake",55,"player2");
+            Jugador jugador3 = new Jugador("LUIS",34,"robgdfodf");
+ 
+            
+            jugador1.PartidaAmistosa(0, jugador2.NumeroDeJugador, 7);
+            jugador1.BuscarPartida(0,7);
+            jugador2.BuscarPartida(0,7);
+
+            jugador1.PosicionarBarcos("A1","A6");
+            jugador1.PosicionarBarcos("B1","B6");
+            jugador2.PosicionarBarcos("E1","E6");
+            jugador2.PosicionarBarcos("F1","F6");
+            
+            int i = 1;
+            while(i <= 6)
+            {
+                jugador1.Atacar($"G{i}");
+                jugador2.Atacar($"A{i}");
+                i+=1;
+            }
+            i = 1;
+            while(i < 6)
+            {
+                jugador1.Atacar($"C{i}");
+                jugador2.Atacar($"B{i}");
+                i+=1;
+            }
+
+            jugador1.Atacar("C6");
+            jugador2.Atacar("B6"); //Termino la partida
+            PerfilUsuario perfil1 = almacenamiento.ObtenerPerfil(jugador1.NumeroDeJugador);
+            List<DatosdePartida> historialpersonal = perfil1.ObtenerHistorialPersonal();
+            PerfilUsuario perfil3 = almacenamiento.ObtenerPerfil(jugador3.NumeroDeJugador);
+            List<DatosdePartida> historialpersonal3 = perfil3.ObtenerHistorialPersonal();
+            PerfilUsuario perfil2 = almacenamiento.ObtenerPerfil(jugador2.NumeroDeJugador);
+            List<DatosdePartida> historialpersonal2 = perfil2.ObtenerHistorialPersonal();
+
+            
+            Assert.AreNotEqual(historialpersonal,historialpersonal3);
+            Assert.AreEqual(historialpersonal,historialpersonal2);
+
+
+            
+            
+        }
+    }
+}
