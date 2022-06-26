d153817b320b73da82d21ed47d99ab68f57bd271
amandaseara <amanda.seara@correo.ucu.edu.uy>
2022-06-26 02:33:08 -0300

    +TestsMensajesPartida

--- test/LibraryTests/MensajesdePartidaTests.cs
+++ test/LibraryTests/MensajesdePartidaTests.cs
@@ -278,7 +278,7 @@ namespace Tests
             PartidasEnJuego remover = PartidasEnJuego.Instance();
             remover.RemoverPartida(partida);
         }
-        /*/// <summary>
+        /// <summary>
         /// Verifica que se devuelven los mensajes correctamente cuando se ataca al agua dos veces seguidas
         /// </summary>
         [Test]
@@ -299,8 +299,115 @@ namespace Tests
             partida.Atacar("D1", 1);
             partida.Atacar("D1", 2);
             string respuesta = partida.Atacar("D1", 1);
-            string expected = "La casilla ya habia sido atacada y contiene Agua";
+            string expected = "La casilla ya habia sido atacada y contiene agua";
             Assert.AreEqual(expected, respuesta);
-        }*/
+        }
+        /// <summary>
+        /// Verifica que se devuelven los mensajes correctamente cuando se ataca un barco
+        /// </summary>
+        [Test]
+        public void AtaqueABarco()
+        {
+            string inicioDelBarco = "A1";
+            string finalDelBarco = "A7";
+            partida.AñadirBarco(inicioDelBarco ,finalDelBarco,1);
+            partida.AñadirBarco(inicioDelBarco ,finalDelBarco,2);
+            string inicioDelBarco1 = "B1";
+            string finalDelBarco1 = "B7";
+            partida.AñadirBarco(inicioDelBarco1 ,finalDelBarco1,1);
+            partida.AñadirBarco(inicioDelBarco1 ,finalDelBarco1,2);
+            string inicioDelBarco2 = "C1";
+            string finalDelBarco2 = "C6";
+            partida.AñadirBarco(inicioDelBarco2 ,finalDelBarco2,1);
+            partida.AñadirBarco(inicioDelBarco2 ,finalDelBarco2,2);
+            string respuesta = partida.Atacar("A1", 1);
+            string expected = "Buen tiro, has atacado a un barco";
+            Assert.AreEqual(expected, respuesta);
+            PartidasEnJuego remover = PartidasEnJuego.Instance();
+            remover.RemoverPartida(partida);
+        }
+        /// <summary>
+        /// Verifica que se devuelven los mensajes correctamente cuando se ataca a un barco dos veces seguidas
+        /// </summary>
+        [Test]
+        public void AtaqueABarco2()
+        {
+            string inicioDelBarco = "A1";
+            string finalDelBarco = "A7";
+            partida.AñadirBarco(inicioDelBarco ,finalDelBarco,1);
+            partida.AñadirBarco(inicioDelBarco ,finalDelBarco,2);
+            string inicioDelBarco1 = "B1";
+            string finalDelBarco1 = "B7";
+            partida.AñadirBarco(inicioDelBarco1 ,finalDelBarco1,1);
+            partida.AñadirBarco(inicioDelBarco1 ,finalDelBarco1,2);
+            string inicioDelBarco2 = "C1";
+            string finalDelBarco2 = "C6";
+            partida.AñadirBarco(inicioDelBarco2 ,finalDelBarco2,1);
+            partida.AñadirBarco(inicioDelBarco2 ,finalDelBarco2,2);
+            partida.Atacar("A1", 1);
+            partida.Atacar("D1", 2);
+            string respuesta = partida.Atacar("A1", 1);
+            string expected = "Has atacado una casilla donde que habia sido atacada anteriormente y contenia una parte de un barco dañado";
+            Assert.AreEqual(expected, respuesta);
+        }
+        /// <summary>
+        /// Verifica que se devuelven los mensajes correctamente cuando se hunde un barco
+        /// </summary>
+        [Test]
+        public void AtaqueHundeBarco()
+        {
+            string inicioDelBarco = "A1";
+            string finalDelBarco = "A7";
+            partida.AñadirBarco(inicioDelBarco ,finalDelBarco,1);
+            partida.AñadirBarco(inicioDelBarco ,finalDelBarco,2);
+            string inicioDelBarco1 = "B1";
+            string finalDelBarco1 = "B7";
+            partida.AñadirBarco(inicioDelBarco1 ,finalDelBarco1,1);
+            partida.AñadirBarco(inicioDelBarco1 ,finalDelBarco1,2);
+            string inicioDelBarco2 = "C1";
+            string finalDelBarco2 = "C6";
+            partida.AñadirBarco(inicioDelBarco2 ,finalDelBarco2,1);
+            partida.AñadirBarco(inicioDelBarco2 ,finalDelBarco2,2);
+            int i = 1;
+            while (i<7)
+            {
+                partida.Atacar($"A{i}",1);
+                partida.Atacar($"A{i}",2);
+                i = i + 1;
+            }
+            string respuesta = partida.Atacar("A7", 1);
+            string expected = "Felicitaciones Has hundido un Barco";
+            Assert.AreEqual(expected, respuesta);
+        }
+        /// <summary>
+        /// Verifica que se devuelven los mensajes correctamente cuando se ataca a un barco hundido
+        /// </summary>
+        [Test]
+        public void AtaqueHundeBarco2()
+        {
+            string inicioDelBarco = "A1";
+            string finalDelBarco = "A7";
+            partida.AñadirBarco(inicioDelBarco ,finalDelBarco,1);
+            partida.AñadirBarco(inicioDelBarco ,finalDelBarco,2);
+            string inicioDelBarco1 = "B1";
+            string finalDelBarco1 = "B7";
+            partida.AñadirBarco(inicioDelBarco1 ,finalDelBarco1,1);
+            partida.AñadirBarco(inicioDelBarco1 ,finalDelBarco1,2);
+            string inicioDelBarco2 = "C1";
+            string finalDelBarco2 = "C6";
+            partida.AñadirBarco(inicioDelBarco2 ,finalDelBarco2,1);
+            partida.AñadirBarco(inicioDelBarco2 ,finalDelBarco2,2);
+            int i = 1;
+            while (i<=7)
+            {
+                partida.Atacar($"A{i}",1);
+                partida.Atacar($"A{i}",2);
+                i = i + 1;
+            }
+            string respuesta = partida.Atacar("A7", 1);
+            string expected = "Has atacado una casilla donde que habia sido atacada anteriormente y contenia una parte de un barco Hundido";
+            Assert.AreEqual(expected, respuesta);
+        }
     }
 }
+
