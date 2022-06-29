d593c4231b4a792778d6a502145a8b841a6b4999
Franco De Stefano <FRANCO.DESTEFANO@correo.ucu.edu.uy>
2022-06-29 00:36:30 -0300

    Actualizo la clase bot para tener menos using

--- src/Library/Bot/Bot.cs
+++ src/Library/Bot/Bot.cs
@@ -1,12 +1,3 @@
-using System;
-using ClassLibrary;
-using System.IO;
-using System.Linq;
-using System.Threading;
-using System.Threading.Tasks;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Options;
 using Telegram.Bot;
 
 namespace ClassLibrary
