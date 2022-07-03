b5b6a3ff6cc2fd35579da871e731e7ea30a23885
Franco De Stefano <FRANCO.DESTEFANO@correo.ucu.edu.uy>
2022-07-02 23:18:13 -0300

    Pequeño arreglo a mensajes enviados

--- src/Library/Handlers/Estado1/MenuHandler.cs
+++ src/Library/Handlers/Estado1/MenuHandler.cs
@@ -27,7 +27,8 @@ namespace ClassLibrary
         {
             if (this.CanHandle(mensaje))
             {
-                respuesta = "Comandos: \n /Remover \n /VerPerfil \n /VerRanking \n /VerHistorial \n /VerHistorialPersonal \n /VisualizarTableros \n /BuscarPartida \n /BuscarPartidaAmistosa \n Para mas informacion, presione: /Ayuda";
+                respuesta = "Estas ubicado en el menu principal.";
+                respuesta += "Estos son los comandos que podras utilizar: \n /Remover \n /VerPerfil \n /VerRanking \n /VerHistorial \n /VerHistorialPersonal \n /VisualizarTableros \n /BuscarPartida \n /BuscarPartidaAmistosa \n Para mas informacion, presione: /Ayuda";
                 return true;
             }
 
