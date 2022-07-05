089e362c0e95f6eec1c7b5bd18073408fe3a9175
Seba-Quintana <SEBASTIAN.QUINTANA@correo.ucu.edu.uy>
2022-07-05 15:36:21 -0300

    nueva excepcion

---
+++ src/Library/Excepciones/CuentaYaExistenteException.cs
@@ -0,0 +1,44 @@
+
+/// <summary>
+/// Excepcion por si un jugador no se encuentra.
+/// </summary>
+[System.Serializable]
+public class CuentaYaExistenteException : System.Exception
+{
+    /// <summary>
+    /// numero del jugador no encontrado
+    /// </summary>
+    public int NumeroDeJugador { get; }
+
+    /// <summary>
+    /// Constructor default
+    /// </summary>
+    public CuentaYaExistenteException() { }
+
+    /// <summary>
+    /// Constructor default
+    /// </summary>
+    /// <param name="message"></param>
+    /// <returns></returns>
+    public CuentaYaExistenteException(string message) : base(message) { }
+
+    /// <summary>
+    /// Constructor default
+    /// </summary>
+    /// <param name="message"></param>
+    /// <param name="inner"></param>
+    /// <returns></returns>
+    public CuentaYaExistenteException(string message, System.Exception inner) : base(message, inner) { }
+
+    /// <summary>
+    /// Constructor adicional para asignar numero de jugador no encontrado al atributo
+    /// y para conseguir el mensaje a mostrar de ser necesario.
+    /// </summary>
+    /// <param name="message"> mensaje a mostrar </param>
+    /// <param name="numerodejugador"> numero del jugador no encontrado </param>
+    /// <returns></returns>
+    public CuentaYaExistenteException(string message, int numerodejugador) : this(message)
+    {
+        NumeroDeJugador = numerodejugador;
+    }
+}
