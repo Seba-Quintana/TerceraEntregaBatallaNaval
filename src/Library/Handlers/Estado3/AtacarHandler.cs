4eddbe1621d267c5daafad89d0b4fba0a1a2ea33
Seba-Quintana <SEBASTIAN.QUINTANA@correo.ucu.edu.uy>
2022-07-05 00:59:24 -0300

    arreglo mensajes historial y atacar

--- src/Library/Handlers/Estado3/AtacarHandler.cs
+++ src/Library/Handlers/Estado3/AtacarHandler.cs
@@ -79,7 +79,7 @@ namespace ClassLibrary
                         if (ResultadoAtacar != "Debe esperar a que el otro jugador lo ataque.")
                             if (ResultadoAtacar != "La coordenada enviada fue invalida")
                             {
-                                mensajeOponente = $"Has sido atacado en {mensaje.Text}\n{Planificador.VerTablero(NumDelJugadorOponente)}";
+                                mensajeOponente = $"Has sido atacado en {mensaje.Text}\n\nTABLERO PROPIO \n\n{Planificador.VerTablero(NumDelJugadorOponente)}";
                                 if (Planificador.PartidaFinalizada(numdelJugador))
                                 {
                                     respuesta += $"\n\nFelicitaciones!!, has ganado la partida. \nLa partida se guardara en su historial e iras al menu principal \nPresiona /menu para mas información";
